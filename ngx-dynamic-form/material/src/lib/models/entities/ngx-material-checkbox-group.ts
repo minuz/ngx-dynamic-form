9d32015c49134c4914c3ca08a0ddfd22e5e2fc48
Fernando Minoru Baba <fernando.fminoru@gmail.com>
2018-05-27 13:16:38 +1000

    Fixed issue related to a checkbox group that was trying to render the wrong form element

--- ngx-dynamic-form/material/src/lib/models/entities/ngx-material-checkbox-group.ts
+++ ngx-dynamic-form/material/src/lib/models/entities/ngx-material-checkbox-group.ts
@@ -3,9 +3,11 @@ import { merge } from 'lodash';
 import { INgxMaterialCheckboxGroup } from '../interface/ngx-material-checkbox-group.interface';
 import { INgxMaterialCheckbox } from '../interface/ngx-material-checkbox.interface';
 import { NgxMaterialGroup } from './ngx-material-group';
+import { FormElementType, InputType } from '@ngx-dynamic-form/core';
 
 export class NgxMaterialCheckboxGroup extends NgxMaterialGroup implements INgxMaterialCheckboxGroup {
 
+  inputType = InputType.Checkbox;
   elements: INgxMaterialCheckbox[] = [];
 
   constructor(opt?: INgxMaterialCheckboxGroup) {
